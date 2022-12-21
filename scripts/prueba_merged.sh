b9e9cae76e8f733861d2aa9490e7d59eb5360719
saramunozl <116674086+saramunozl@users.noreply.github.com>
2022-12-21 14:15:21 +0100

    Delete prueba_merged.sh

--- scripts/prueba_merged.sh
+++
@@ -1,5 +0,0 @@
-# Merge the samples into a single file
-for sid in $(ls data/*.fastq.gz | cut -d"-" -f1 | sed "s:data/::" | sort | uniq)
-do
-    bash scripts/merge_fastqs.sh data out/merged $sid
-done
